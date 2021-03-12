7dbf2a4f9099a31dead3f560c6121a60f5091a1c
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-11 19:07:56 -0600

    have buttons, need to figure out how to move img.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,29 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener("load", (event) => {
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight")
+    let flightStatus = document.getElementById("flightStatus")
+    let takeoffButton = document.getElementById("takeoff")
+    let landButton =document.getElementById("landing")
+    let shuttleBackground = document.getElementById("shuttleBackground")
+    let upButton = document.getElementById("upButton")
+    let downButton = document.getElementById("downButton")
+    let leftButton = document.getElementById("leftButton")
+    let rightButton = document.getElementById("rightButton")
+
+    takeoffButton.addEventListener("click", () => {
+        let confirm = window.confirm("Is the shuttle ready for liftoff?")
+        if (confirm) {
+            flightStatus.innerHTML = "Shuttle in flight!"
+            shuttleBackground.style.backgroundColor = "blue"
+            spaceShuttleHeight.innerHTML += 10000
+        }
+    })
+
+    landButton.addEventListener("click", () => {
+        alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed."
+        shuttleBackground.style.backgroundColor = "green"
+    })
+})
